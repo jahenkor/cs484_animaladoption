4e5b32c2f14339c09c4455c0f112900c5f5ff896
jahenkor <juliusahenkora@gmail.com>
2019-04-17 12:40:20 -0400

    Updated preprocessing steps

--- model.py
+++ model.py
@@ -9,21 +9,42 @@ from sklearn.model_selection import train_test_split
 def main():
 
 
-    animal_intake, animal_outcome = loadData()
+    animal_intake, animal_outcome = LoadData()
+    PreprocessData(animal_outcome,animal_intake)
 
 
 
     return 0
 
-def loadData():
+def LoadData():
 
     animal_intake = pd.read_csv("dataset/Austin_Animal_Center_Intakes.csv")
     animal_outcome = pd.read_csv("dataset/Austin_Animal_Center_Outcomes.csv")
 
+    #removing spaces in fieldnames so their easier to work with
+    animal_intake.columns = [x.replace(' ', '') for x in animal_intake.columns]
+    animal_outcome.columns = [x.replace(' ','') for x in animal_outcome.columns]
+
 
     print(animal_outcome)
     print(animal_intake)
+    print(animal_intake.columns)
 
     return animal_intake, animal_outcome
+
+def PreprocessData(animal_outcome,animal_intake):
+
+    #Add/Remove/update fields
+
+
+    dropList = ['FoundLocation']
+    animal_intake.drop(columns=dropList,inplace=True)
+    print(animal_intake)
+
+
+
+    #Join Tables by AnimalID
+
+
 
 main()
