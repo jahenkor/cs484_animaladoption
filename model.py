9c1aa5371676976b65d4a312252867c9bada76b7
jahenkor <juliusahenkora@gmail.com>
2019-04-25 22:13:56 -0400

    Another Update

--- model.py
+++ model.py
@@ -33,30 +33,57 @@ def LoadData():
     print(animal_intake)
     print(animal_intake.columns)
 
+    #Sort both datasets
+    animal_intake = animal_intake.sort_values('AnimalID')
+    animal_outcome = animal_outcome.sort_values('AnimalID')
+
+
     #Train/test split
-    trainI, testI = train_test_split(animal_intake, train_size = 0.5,shuffle=False)
-    trainO, testO = train_test_split(animal_outcome,train_size=0.5, shuffle=False)
+    trainI, testI = train_test_split(animal_intake, train_size = 0.01,shuffle=False)
+    trainO, testO = train_test_split(animal_outcome,train_size=0.01, shuffle=False)
 
     return animal_intake, animal_outcome
 
 def BreakDates(animal_intake):
 
 # Break dates, include Day of Week, Time
-    animal_intake['Date'], animal_intake['Time'] = animal_intake['DateTime'].str.split(' ',1).str
-    animal_intake['DayOfWeek'] = animal_intake['Date'].copy()
-    animal_intake.drop(['DateTime'],axis=1, inplace=True)
+    animal_intake['Date_x'], animal_intake['Time_x'] = animal_intake['DateTime_x'].str.split(' ',1).str
+    animal_intake['Date_y'], animal_intake['Time_y'] = animal_intake['DateTime_y'].str.split(' ',1).str
+
+    animal_intake['DayOfWeek_x'] = animal_intake['Date_x'].copy()
+    animal_intake['DayOfWeek_y'] = animal_intake['Date_y'].copy()
+
+    animal_intake.drop(['DateTime_x'],axis=1, inplace=True)
+    animal_intake.drop(['DateTime_y'],axis=1, inplace=True)
+
+    print(animal_intake['Date_x'])
+    print(animal_intake['DayOfWeek_x'])
 
 
     j = 0
-    for i in animal_intake['DayOfWeek']:
+    for i in animal_intake['DayOfWeek_x']:
+        if i == "nan":
+            continue
+        print(j)
+        j += 1
+        print(i)
+        dateSplit = i.split('/')
+        #print(dateSplit)
+        real_date = date(int(dateSplit[2]),int(dateSplit[0]),int(dateSplit[1]))
+        #print(real_date.weekday())
+        animal_intake['DayOfWeek_x'] = real_date.weekday()
+    for i in animal_intake['DayOfWeek_y']:
         print(j)
         j += 1
         dateSplit = i.split('/')
         #print(dateSplit)
         real_date = date(int(dateSplit[2]),int(dateSplit[0]),int(dateSplit[1]))
         #print(real_date.weekday())
-        animal_intake['DayOfWeek'] = real_date.weekday()
-    print(animal_intake['DayOfWeek'])
+        animal_intake['DayOfWeek_y'] = real_date.weekday()
+
+    print(animal_intake['DayOfWeek_x'])
+    print(animal_intake['DayOfWeek_y'])
+
 
 
  #       #Remove backslashes from Date
@@ -72,23 +99,66 @@ def BreakDates(animal_intake):
 
 #Takes a loooong time
 def PreprocessData(animal_outcome,animal_intake):
+
     print(animal_outcome.columns)
     print(animal_outcome['AnimalID'])
+    print(animal_intake['AnimalID'])
     #ids = animal_outcome['AnimalID']
     #print(animal_outcome[ids.isin(ids[ids.duplicated()])].sort_values("AnimalID"))
     #print(animal_outcome.groupby("AnimalID").count())
-
-
-    #Add/Remove/update fields
-
-    dropList = ['FoundLocation']
-    animal_intake.drop(columns=dropList,inplace=True)
 
     #Cats,Birds,Dogs only
     animal_intake = animal_intake[animal_intake.AnimalType != "Other"]
     animal_outcome = animal_outcome[animal_outcome.AnimalType != "Other"]
 
+   #Name Field
+    #Boolean
+    #animal_intake['Name'].dropna(axis=0, how='any', inplace=True)
 
+    animal_intake['Name'].fillna('No')
+    names = []
+    for i in range(len(animal_intake['Name'])):
+        print("Changing Name: %d" % i)
+        if animal_intake['Name'].iloc[i] != 'No':
+            #animal_intake['Name'].iloc[i] = 'Yes'
+            names.append("Yes")
+        else:
+            names.append("No")
+    animal_intake['Name'] = names
+
+
+
+    #End Name conversion
+
+
+
+
+    #Add/Remove/update fields
+
+
+
+
+
+    dropListIntake = ['FoundLocation','MonthYear']
+    #remove redundant features from outcome dataset
+    dropListOutcome = ['DateofBirth','MonthYear','OutcomeSubtype','Name','AnimalType',"Color","Breed"]
+    animal_intake.drop(columns=dropListIntake,inplace=True)
+    animal_outcome.drop(columns=dropListOutcome, inplace=True)
+    animal_outcome.dropna(inplace=True)
+    animal_intake.dropna(inplace=True)
+    print(animal_intake['Name'])
+    print(animal_intake['DateTime'])
+    print(animal_outcome['DateTime'])
+
+
+    #Remove missing values
+    animal_intake.dropna(inplace=True)
+    animal_outcome.dropna(inplace=True)
+
+
+
+
+    '''
     isAggressive = []
     isMix = []
     for i in range(len(animal_intake['Breed'])):
@@ -108,52 +178,113 @@ def PreprocessData(animal_outcome,animal_intake):
     exit(0)
     print("removedup intake")
     print(animal_intake)
-
+    '''
 
 
     #Name Frequency
-    '''
     animal_intake['NameFreq'] = np.ones((animal_intake.shape)[0])
 
 
-
     #Aggregate Name Frequency
-   # animal_intake['NameFreq'] = animal_intake.groupby('AnimalID')['NameFreq'].count()
+    name_freq = animal_intake.groupby('AnimalID')['NameFreq'].count()
+    print(name_freq)
+
 
     #Gets last entry for animalID
-    print("PreDup")
     animal_outcome = animal_outcome.sort_values('DateTime').drop_duplicates('AnimalID',keep='last')
     animal_intake = animal_intake.sort_values('DateTime').drop_duplicates('AnimalID',keep='last')
-    print("PostDup  intake")
-    print(animal_intake)
-    print(len(animal_intake))
+    print(name_freq)
+    animal_intake = animal_intake.merge(name_freq,on=['AnimalID'],how='right')
+    animal_intake = animal_intake.rename(index=str, columns={"NameFreq_y":"NumberOfAdmits"})
+    animal_intake
+    animal_intake = animal_intake.drop(['DateTime_y'],axis=1)
 
-    #animal_intake['NameFreq'] = animal_intake.groupby('AnimalID')['NameFreq']
-    print(animal_intake['AnimalID'])
-    print(animal_outcome['AnimalID'])
-    common = pd.concat([animal_intake['AnimalID'],animal_outcome['AnimalID']]).drop_duplicates(keep=False)
-    print(common)
+    print(animal_intake.sort_values('AnimalID'))
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(animal_intake[:10])
+
 
     exit(0)
+
+    #print("PostDup  intake")
+    #print(animal_intake)
+    #print(len(animal_intake))
+    #name_freq = np.array(animal_intake.groupby('AnimalID')['NameFreq'])
+    print(name_freq)
+    #print(animal_intake['AnimalID'])
+    #print(animal_outcome['AnimalID'])
+    #common = pd.concat([animal_intake['AnimalID'],#animal_outcome['AnimalID']]).drop_duplicates(keep=False)
+
+    #print(common)
+
 
     #with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
         #print(animal_outcome[:10])
         #print(animal_intake[:10])
 
-
     #Join fields
-    dataset = pd.merge(animal_outcome,animal_intake,on=["AnimalID","Breed","Color","AnimalType","Name"], how="outer")
-    #with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-        #print(dataset[:10])
+    dataset = pd.merge(animal_outcome,animal_intake,on=["AnimalID"], how="right").drop_duplicates()
+    #dataset = pd.merge(dataset, name_freq, on=["AnimalID"], how="inner")
 
+
+    print(dataset['AnimalID'])
+    print(name_freq.shape)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(dataset[:10])
+    print(animal_outcome.shape)
+    print(animal_intake.shape)
+    print(dataset.shape)
+    print(name_freq.shape)
+    dataset.dropna(inplace=True)
+
+
+
+
+
+
+    #BreakDates(dataset)
+#    dataset = dataset.sort_values('Date_x').drop_duplicates('AnimalID',keep='last')
+
+
+    dataset = dataset.sort_values('AnimalID')
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(dataset[:10])
+
+    print(dataset)
+    print(dataset.columns)
+
+    exit(0)
+
+
+
+
+    #dataset = dataset[['AnimalID','DateTime_x']].drop_duplicates(keep='last'))
+    #print(dataset[['AnimalID','DateTime_y']].drop_duplicates(keep='last'))
+
+
+
+
+    #dataset[['AnimalID','Date_x','Time_x']] = dataset[['AnimalID','Date_x','Time_x']].drop_duplicates(keep='last')
+
+
+    #dataset[['AnimalID','Date_y','Time_y']] = dataset[['AnimalID','Date_y','Time_y']].drop_duplicates(keep='last')
+
+    #dataset.dropna(inplace=True)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(dataset[:10])
+
+
+
+    print(dataset)
+    print(animal_intake['DateTime'])
+#    print(dataset['MonthYear_x'].drop_duplicates(keep='last'))
+    exit(0)
     #print(name_freq)
     #dataset['NameFreq'] = name_freq
     #print(dataset)
     #print(dataset.columns)
 
 
-    BreakDates(animal_outcome)
-    BreakDates(animal_intake)
 
 
 
@@ -161,34 +292,13 @@ def PreprocessData(animal_outcome,animal_intake):
 
 
 
-
-
-    for i in range(5):
-
-        print(dataset.iloc[i])
-
-    print(dataset)
-    animalID = dataset.groupby(['AnimalID'])
-    print((animalID.count())['NameFreq'])
-    print(dataset.iloc[0])
-'''
-    #Add/Remove/update fields
-    '''
-
-    dropList = ['FoundLocation']
-    animal_intake.drop(columns=dropList,inplace=True)
-
-    #Cats,Birds,Dogs only
-    animal_intake = animal_intake[animal_intake.AnimalType != "Other"]
-    print(animal_intake)
-    '''
 
 
 
     #Fix Age (Normalize values to age in days)
     '''
     for i in range(len(animal_intake['AgeuponIntake'])):
-        if "months" in animal_intake['AgeuponIntake'].iloc[i] or "month" in animal_intake['AgeuponIntake'].iloc[i]:
+        eif "months" in animal_intake['AgeuponIntake'].iloc[i] or "month" in animal_intake['AgeuponIntake'].iloc[i]:
             print(i)
             fixed_time = int((animal_intake['AgeuponIntake'].iloc[i])[0]) * 30.417
             animal_intake['AgeuponIntake'].iloc[i] = str(fixed_time) + " days"
